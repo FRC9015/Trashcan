5b5b65c79256aa591aac77bb6a0231651c066951
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-09-23 20:22:34 -0400

    working thingy

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,29 @@
 
 #include <driver/dac.h>
 
+void onConnect()
+{
+  Serial.println("Connected!.");
+}
+
+void onDisConnect()
+{
+  Serial.println("Disconnected!.");
+}
+
 void setup()
 {
   // Enable Serial output
   Serial.begin(115200);
 
-  // Initialize PS4 Controller
-  // TODO: Find and use correct MAC address
-  PS4.begin();
-
   // Enable DAC pins
   dac_output_enable(DAC_CHANNEL_1); // GPIO25
   dac_output_enable(DAC_CHANNEL_2); // GPIO26
+
+  PS4.attachOnConnect(onConnect);
+  PS4.attachOnDisconnect(onDisConnect);
+
+  PS4.begin();
 
   Serial.println("Setup complete");
 }
@@ -24,8 +35,12 @@ void setup()
 void loop()
 {
   // Read PS4 Controller Outputs
-  int8_t xPos = PS4.LStickX();
-  int8_t yPos = PS4.LStickY();
+  int8_t rawXpos = PS4.LStickX();
+  int8_t rawYpos = PS4.LStickY();
+
+  // Convert to 0-255 range
+  uint8_t xPos = map(rawXpos, -128, 127, 0, 255);
+  uint8_t yPos = map(rawYpos, -128, 127, 0, 255);
 
   // Log out values to Serial console
   Serial.print("X: ");
@@ -36,4 +51,6 @@ void loop()
   // Write values to DAC (experimental)
   dac_output_voltage(DAC_CHANNEL_1, xPos);
   dac_output_voltage(DAC_CHANNEL_2, yPos);
+
+  delay(1000);
 }
